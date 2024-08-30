af8aec28313983923707b45502292d8043446015
UnderMaster3107 <murik6269@gmail.com>
2024-08-30 23:12:52 +0300

    аптейт программы
    теперь программа не только вычисляет сумму, но и так же проверяет на равенство

--- main.cpp
+++ main.cpp
@@ -2,14 +2,29 @@
 #include <vector>
 
 int main() {
-    std::vector<int> numbers{ 1, 2, 3, 4, 5 };
+    std::vector<int> numbers{1, 2, 3, 4, 5};
     int sum = 0;
+    bool allEqual = true;
+
+    
+    int firstElement = numbers[0];
 
     for (int number : numbers) {
         sum += number;
+
+        
+        if (number != firstElement) {
+            allEqual = false;
+        }
     }
 
     std::cout << "Сумма элементов вектора: " << sum << std::endl;
+
+    if (allEqual) {
+        std::cout << "Все элементы вектора равны." << std::endl;
+    } else {
+        std::cout << "Элементы вектора не равны." << std::endl;
+    }
 
     return 0;
 }
